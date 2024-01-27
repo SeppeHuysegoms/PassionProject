165b88944eeda61b3a20c35860398d88f607f5c5
SeppeHuysegoms <seppe.huysegoms@student.howest.be>
2024-01-27 15:38:01 +0100

    Arduino test code whole panel

---
+++ ArduinoFinal/ArduinoCode/ArduinoCode.ino
@@ -0,0 +1,97 @@
+#include <NewPing.h>
+
+#define SONAR_NUM 3
+#define MAX_DISTANCE 20
+
+const int buttonPin1 = 45;
+const int buttonPin2 = 49;
+const int buttonPin3 = 53;
+
+// variables will change:
+int buttonState1 = 0;
+int oldButtonState1 = LOW;
+int value1 = 0;
+
+int buttonState2 = 0;
+int oldButtonState2 = LOW;
+int value2 = 0;
+
+int buttonState3 = 0;
+int oldButtonState3 = LOW;
+int value3 = 0;
+
+NewPing sonar[SONAR_NUM] = {
+  NewPing(4, 5, MAX_DISTANCE),
+  NewPing(6, 7, MAX_DISTANCE),
+  NewPing(8, 9, MAX_DISTANCE),
+};
+
+void setup() {
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(buttonPin3, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int sensorValue1 = analogRead(A8);
+  int sensorValue2 = analogRead(A9);
+  int sensorValue3 = analogRead(A10);
+  int sensorValue4 = analogRead(A11);
+
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+
+  if (buttonState1 != oldButtonState1 && buttonState1 == HIGH) {
+    value1 = (value1 == 0 ? 1 : 0);
+  }
+  oldButtonState1 = buttonState1;
+
+  if (buttonState2 != oldButtonState2 && buttonState2 == HIGH) {
+    value2 = (value2 == 0 ? 1 : 0);
+  }
+  oldButtonState2 = buttonState2;
+
+  if (buttonState3 != oldButtonState3 && buttonState3 == HIGH) {
+    value3 = (value3 == 0 ? 1 : 0);
+  }
+  oldButtonState3 = buttonState3;
+
+
+  Serial.print("ultrasone1:");
+  Serial.print(sonar[0].ping_cm());
+  Serial.print(", ");
+  Serial.print("ultrasone2:");
+  Serial.print(sonar[1].ping_cm());
+  Serial.print(", ");
+  Serial.print("ultrasone3:");
+  Serial.print(sonar[2].ping_cm());
+  Serial.print(", ");
+  Serial.print("button1:");
+  Serial.print(value1);
+  Serial.print(", ");
+  Serial.print("button2:");
+  Serial.print(value2);
+  Serial.print(", ");
+  Serial.print("button3:");
+  Serial.print(value3);
+  Serial.print(", ");
+  Serial.print("potentio1:");
+  Serial.print(sensorValue1);
+  Serial.print(", ");
+  Serial.print("potentio2:");
+  Serial.print(sensorValue2);
+  Serial.print(", ");
+  Serial.print("potentio3:");
+  Serial.print(sensorValue3);
+  Serial.print(", ");
+  Serial.print("potentio4:");
+  Serial.print(sensorValue4);
+  Serial.print(", ");
+  Serial.println();
+
+
+  delay(100);
+}
